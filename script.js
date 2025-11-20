c569909b75c97c1e0eed5fe9f3ac0793a6847e52
gnitcmidexams <midexams@gniindia.org>
2025-11-20 14:38:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -757,7 +757,7 @@ async function generateWord(questions, paperDetails, monthyear, midTermText, dow
                                 children: [
                                     new TableCell({
                                         width: { size: 10, type: WidthType.PERCENTAGE },
-                                        children: [new TextRun({ text: `${index + 1}`,  font: 'Times New Roman',alignment: AlignmentType.JUSTIFY, size: 24 })]
+                                        children: [new Paragraph({ text: `${index + 1}`, alignment: AlignmentType.JUSTIFY, font: 'Times New Roman', size: 24 })]
                                         // children: [new Paragraph({children:[new TextRun({ text: `${index + 1}`,  font: 'Times New Roman', size: 24 })],alignment: AlignmentType.JUSTIFY})]
                                     }),
                                     new TableCell({
